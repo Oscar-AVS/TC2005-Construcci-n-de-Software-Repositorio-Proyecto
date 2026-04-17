892ecaee2311e6032c1a281e2952ccc59467b650
lakshjara <lakshjara@gmail.com>
2026-04-16 23:22:56 -0600

    feat(project-manager-controller): add basic view routes (back)

--- controllers/project-manager.controller.js
+++ controllers/project-manager.controller.js
@@ -4,3 +4,39 @@
 const User    = require('../models/user.model');
 const Project = require('../models/project.model');
 const bcrypt  = require('bcrypt');
+
+exports.getDashboard = (req, res) => {
+  res.render('project-manager/dashboard', {
+    title: 'PM Dashboard',
+    role: 'project-manager',
+    currentPage: 'dashboard',
+  });
+};
+
+exports.getReports = (req, res) => {
+  res.render('project-manager/reports', {
+    title: 'Reports',
+    role: 'project-manager',
+    currentPage: 'reports',
+  });
+};
+
+exports.getLog = (req, res) => {
+  res.render('shared/log', {
+    title: 'My Log',
+    role: 'project-manager',
+    currentPage: 'log',
+    logs: [],
+    projects: [],
+    filters: {},
+    csrfToken: req.csrfToken(),
+  });
+};
+
+exports.getSelfReview = (req, res) => {
+  res.render('shared/self-review', {
+    title: 'Self Review',
+    role: 'project-manager',
+    currentPage: 'self-review',
+  });
+};
