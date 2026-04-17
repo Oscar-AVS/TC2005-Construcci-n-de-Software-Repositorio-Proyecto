d39948f66b5c134ddb3e34e58277c5c399ebd852
lakshjara <lakshjara@gmail.com>
2026-04-16 23:41:07 -0600

    chore(project-manager-routes): initialize routes and controller import (back)

--- routes/project-manager.routes.js
+++ routes/project-manager.routes.js
@@ -1,19 +1,6 @@
 /**
  * Project Manager Routes
  */
-
-const express = require('express');
-const router = express.Router();
+const express      = require('express');
+const router       = express.Router();
 const pmController = require('../controllers/project-manager.controller');
-
-router.get('/dashboard', pmController.getDashboard);
-router.get('/projects', pmController.getProjects);
-router.get('/project/:id', pmController.getProjectDetail);
-router.get('/reports', pmController.getReports);
-router.get('/log', pmController.getLog);
-router.get('/self-review', pmController.getSelfReview);
-router.get('/profile', pmController.getProfile);
-router.post('/profile/slack', pmController.postSlack);
-router.post('/profile/password', pmController.postPassword);
-
-module.exports = router;
