3d4b20742bc1acff7fe48e6d407194e361ba71c3
lakshjara <lakshjara@gmail.com>
2026-04-16 22:27:28 -0600

    chore(project-model): initialize project model structure

--- models/project.model.js
+++ models/project.model.js
@@ -1,19 +1,9 @@
 /**
  * Project model.
- * Handles database operations for project assignments.
+ * Handles all database operations for project management.
  */
-
 const db = require('../util/database');
 
 module.exports = class Project {
-static fetchAllByEmployee(id_user) {
-    return db.execute(
-    `SELECT p.*, t.team_name, ua.id_team
-    FROM project p
-    JOIN user_assignment ua ON p.id_project = ua.id_project
-    JOIN team t ON ua.id_team = t.id_team
-    WHERE ua.id_user = ?`,
-    [id_user]
-    );
-}
+
 };
