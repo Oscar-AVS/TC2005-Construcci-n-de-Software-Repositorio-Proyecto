b7325c4a6042f89657c69ad5fe631148a2abb1c8
pacoa1 <a01706242@tec.mx>
2026-04-21 19:46:33 -0600

    Fix Merge conflict

--- models/project.model.js
+++ models/project.model.js
@@ -29,6 +29,19 @@ module.exports = class Project {
     );
   }
 
+  static fetchAllByEmployee(id_user) {
+    return db.execute(
+      `SELECT p.id_project, p.project_name, p.description, p.progress_status, p.start_date,
+              COALESCE(t.team_name, 'Sin equipo asignado') AS team_name, ua.id_team
+       FROM project p
+       JOIN user_assignment ua ON p.id_project = ua.id_project
+       LEFT JOIN team t ON ua.id_team = t.id_team
+       WHERE ua.id_user = ?
+       ORDER BY p.project_name`,
+      [id_user]
+    );
+  }
+
   static fetchOne(id_project) {
     return db.execute(
       `SELECT * FROM project WHERE id_project = ?`,
