57c81b0e2bc72734ff3f299ff0a54777381ae382
Oscar-AVS <alexandervilchis638@gmail.com>
2026-04-24 10:08:36 -0600

    fix(goals): enable delete goal modal action

--- routes/manager.routes.js
+++ routes/manager.routes.js
@@ -45,7 +45,7 @@ router.post('/profile/slack', managerController.postSlack);
 router.post('/profile/password', managerController.postPassword);
 router.post('/goals/:id/link-project', managerController.linkProjectToGoal);
 router.post('/goals/:id/unlink-project', managerController.unlinkProjectFromGoal);
-router.delete('/goals/:id', managerController.deleteGoal);
+router.post('/goals/:id/delete', managerController.deleteGoal);
 router.post('/goals/:id/impact-summary', managerController.generateGoalImpactSummary);
 router.post('/goals/:id/export-pdf', managerController.exportGoalImpactPDF);
 
