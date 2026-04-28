f67bfacddb6a4a50d87a5ddf188f23440633740e
pacoa1 <a01706242@tec.mx>
2026-04-28 10:54:08 -0600

    feat (admin):Implementación de control de acceso por privilegios y gestión de roles

--- routes/employee.routes.js
+++ routes/employee.routes.js
@@ -8,6 +8,7 @@ const rateLimit = require('express-rate-limit');
 const router = express.Router();
 
 const employeeController = require('../controllers/employee.controller');
+const { requirePrivilege } = require('../util/is-auth');
 
 const selfReviewLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
@@ -20,18 +21,23 @@ const selfReviewLimiter = rateLimit({
 });
 
 router.get('/dashboard', employeeController.getDashboard);
+
 router.get('/log', employeeController.getLog);
 router.post('/log', employeeController.postLog);
 router.post('/log/edit', employeeController.putLog);
 router.post('/log/delete', employeeController.deleteLog);
+
 router.get('/achievements', employeeController.getAchievements);
 router.post('/achievements', employeeController.postAchievement);
 router.post('/achievements/delete', employeeController.deleteAchievement);
 router.post('/achievements/edit', employeeController.editAchievement);
+
 router.get('/self-review', employeeController.getSelfReview);
 router.get('/self-review/generate', selfReviewLimiter, employeeController.generateSelfReview);
 router.post('/self-review/export-pdf', selfReviewLimiter, employeeController.exportSelfReviewPDF);
+
 router.get('/projects', employeeController.getProjects);
+
 router.get('/profile', employeeController.getProfile);
 router.post('/profile/slack', employeeController.postSlack);
 router.post('/profile/password', employeeController.postPassword);
